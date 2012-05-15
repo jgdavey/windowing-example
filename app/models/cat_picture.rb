d447f9b5efa458d7180f3bdb801a21d8cf4dfcce
Joshua Davey <josh@joshuadavey.com>
2012-05-15 14:05:02 -0500

    Add 'cheapest_per_category' Ruby implementation

--- app/models/cat_picture.rb
+++ app/models/cat_picture.rb
@@ -1,4 +1,10 @@
 class CatPicture < ActiveRecord::Base
   attr_accessible :category_id, :description, :name, :price
   belongs_to :category
+
+  def self.cheapest_per_category
+    all.group_by(&:category_id).map do |category_id, subset|
+      subset.sort_by { |pic| [pic.price, pic.name] }.first
+    end.compact
+  end
 end
--- spec/models/cat_picture_spec.rb
+++ spec/models/cat_picture_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe CatPicture do
+  describe '.cheapest_per_category' do
+    let!(:category2) { Fabricate(:category) }
+    let!(:category1) { Fabricate(:category) }
+
+    it "returns a list, with only the cheapest for its category" do
+      pic1 = Fabricate(:cat_picture, price: 8.5,  category_id: category1.id)
+      pic2 = Fabricate(:cat_picture, price: 12.5, category_id: category1.id)
+      pic3 = Fabricate(:cat_picture, price: 4.5, category_id: category2.id)
+      pic4 = Fabricate(:cat_picture, price: 9.5, category_id: category2.id)
+      CatPicture.cheapest_per_category.should == [pic1, pic3]
+    end
+
+    context "when two pictures have the same price" do
+      it "returns only the first one, when sorted by name" do
+        pic1 = Fabricate(:cat_picture, price: 8.5,  category_id: category1.id)
+        pic2 = Fabricate(:cat_picture, price: 12.5, category_id: category1.id)
+        pic3 = Fabricate(:cat_picture, name: "Zeke", price: 4.5, category_id: category2.id)
+        pic4 = Fabricate(:cat_picture, name: "Alex", price: 4.5, category_id: category2.id)
+        CatPicture.cheapest_per_category.should == [pic1, pic4]
+      end
+    end
+  end
 end
